4a50c530c93900c239c0b8212fd2278d7f688b41
Novrian Y.F <me@novrian.info>
2013-07-30 04:26:09 +0800

    Done Project

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,12 @@ return array(
 
 	'modules' => array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'1234',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -58,6 +56,7 @@ return array(
 			'username' => 'yii_trakerx',
 			'password' => 'cQbdHyTEdQ95ndLN',
 			'charset' => 'utf8',
+            'tablePrefix' => 'trk_'
 		),
 
 		'errorHandler' => array(
